69f56d0d68eef6981ea2eb59c8fa26742e061262
hikidashi <82632238+hikidashi@users.noreply.github.com>
2021-09-10 12:52:39 +0200

    fix vite build base path

--- display/vite.config.js
+++ display/vite.config.js
@@ -1,14 +1,18 @@
 import { readFileSync } from 'node:fs';
 
-import nodePolyfills from 'rollup-plugin-polyfill-node';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	base: '/game-scripts/',
 	esbuild: {
 		tsconfigRaw: readFileSync('tsconfig.display.json', 'utf8'),
 	},
 	json: {
 		stringify: true,
 	},
-	plugins: [{ ...nodePolyfills(), pre: true }],
+	server: {
+		fs: {
+			allow: ['components', 'worker'],
+		},
+	},
 });
